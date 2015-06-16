2df559d8b11d7a537585f969b9b7bb994e080215
Ben Goldberg <jediben97@gmail.com>
2015-06-16 11:18:52 -0400

    Added comments

--- twoStage.h
+++ twoStage.h
@@ -4,7 +4,9 @@
 //code to make the robot wait for controller button to be pressed
 int buttonWait() {
 	int startWait = time100[T1] / 10;
+	//stop motors
 	postAuton();
+	//wait for button press
 	while(true) {
 		if(vexRT[Btn8L] == 1)
 			break;
@@ -20,20 +22,23 @@ void twoStage() {
 	int startTime;
 	int upTime;
 
-	//wait for
+	//wait for button on robot to be pressed
 	while(true) {
 		if(SensorValue(dgtl1) == 1) {
 			startTime = time100[T1] / 10;
 			break;
 		}
 	}
+	//Run auton function on a loop for 30 seconds
 	while(getTime() - startTime <= AUTONTIME) {
 		writeDebugStreamLine("%i : %i : %i", upTime, AUTONTIME, upTime <= AUTONTIME);
 		auton();
 	}
 	startTime += buttonWait();
+	//Run teleop function on a loop for 120 seconds
 	while(getTime() - startTime > AUTONTIME && getTime() - startTime <= TELEOPTIME) {
 		teleop();
 	}
+	//stop motors
 	postTeleop();
 }
