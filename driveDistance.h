3bed0ea7f5c98507e3adb610fbb1c57f41f94c10
Ben Goldberg <jediben97@gmail.com>
2015-06-16 11:29:27 -0400

    Update driveDistance.h

--- driveDistance.h
+++ driveDistance.h
@@ -6,23 +6,27 @@ void driveDistance(int distance, float wheel_circumference, short leftMotor, sho
 
 	nMotorEncoder[leftMotor] = 0;
 	nMotorEncoder[rightMotor] = 0;
+	//calculate distance degrees to travel based on desired distance and wheel circumference
 	int distanceToTravel = (360 / wheel_circumference) * distance;
 
 	writeDebugStreamLine("Sensor Value: %i Distance: %i\n", SensorValue(leftEncoder), distanceToTravel);
 
+	//Go until desired distance is traveled
 	while(SensorValue(getEncoderForMotor(leftMotor)) > distanceToTravel) {
 
 		writeDebugStreamLine("Left Encoder:%i - Right Encoder:%i", SensorValue(getEncoderForMotor(leftMotor)), SensorValue(getEncoderForMotor(rightMotor)));
+		
+		//Go straight
 		if(SensorValue(getEncoderForMotor(leftMotor)) == SensorValue(getEncoderForMotor(rightMotor))) {
 
 			motor[leftMotor] = 127;
 			motor[rightMotor] = 127;
-
+		//Correct if pulling right
 		} else if(SensorValue(getEncoderForMotor(leftMotor)) > SensorValue(getEncoderForMotor(rightMotor))) {
 
 			motor[leftMotor] = 114;
 			motor[rightMotor] = 127;
-
+		//Correct if pulling left
 		} else if(SensorValue(getEncoderForMotor(leftMotor)) < SensorValue(getEncoderForMotor(rightMotor))) {
 
 			motor[leftMotor] = 127;
@@ -30,6 +34,9 @@ void driveDistance(int distance, float wheel_circumference, short leftMotor, sho
 
 		}
 	}
+	//stop
+	motor[leftMotor] = 0;
+	motor[rightMotor] = 0;
 	nMotorEncoder[leftMotor] = 0;
 	nMotorEncoder[rightMotor] = 0;
 }
