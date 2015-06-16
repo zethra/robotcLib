99a9c369d12074c762f2bc3cd4a56d7d7e3408fc
Ben Goldberg <jediben97@gmail.com>
2015-06-16 11:30:23 -0400

    Update drive.h

--- drive.h
+++ drive.h
@@ -1,14 +1,15 @@
 void drive(int speed) {
+	//Go straight
 	if(SensorValue(getEncoderForMotor(leftMotor)) == SensorValue(getEncoderForMotor(rightMotor))) {
 
 		motor[leftMotor] = speed;
 		motor[rightMotor] = speed;
-
+	//Correct if pulling right
 	} else if(SensorValue(getEncoderForMotor(leftMotor)) > SensorValue(getEncoderForMotor(rightMotor))) {
 
 		motor[leftMotor] = speed * 0.2;
 		motor[rightMotor] = speed * 1.2;
-
+	//Correct if pulling left
 	} else if(SensorValue(getEncoderForMotor(leftMotor)) < SensorValue(getEncoderForMotor(rightMotor))) {
 
 		motor[leftMotor] = speed * 1.2;
